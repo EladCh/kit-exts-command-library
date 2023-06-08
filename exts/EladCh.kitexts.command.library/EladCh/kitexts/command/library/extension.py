0c26941d2d6de499bf35dac549224bdef2860779
Elad Chamilevsky <elad.ch.631@gmail.com>
2023-06-08 20:47:14 +0300

    initial

---
+++ exts/EladCh.kitexts.command.library/EladCh/kitexts/command/library/extension.py
@@ -0,0 +1,56 @@
+import omni.ext
+import omni.ui as ui
+import omni.kit.commands
+import omni.usd
+from typing import List
+
+# Any class derived from `omni.ext.IExt` in top level module (defined in `python.modules` of `extension.toml`) will be
+# instantiated when extension gets enabled and `on_startup(ext_id)` will be called. Later when extension gets disabled
+# on_shutdown() is called.
+class EladchKitextsCommandLibraryExtension(omni.ext.IExt):
+    # ext_id is current extension id. It can be used with extension manager to query additional information, like where
+    # this extension is located on filesystem.
+    def on_startup(self, ext_id):
+        print("[EladCh.kitexts.command.library] EladCh kitexts command library startup")
+
+        self._count = 0
+
+        self._window = ui.Window("My Window", width=300, height=300)
+        with self._window.frame:
+            with ui.VStack():
+
+                def on_click():
+                    prim_paths = get_selection()
+                    omni.kit.commands.execute('ScaleIncrement', prim_paths=prim_paths)
+
+                print("Scale up!")
+
+
+                ui.Button("Increase", clicked_fn=lambda: on_click())
+
+    def on_shutdown(self):
+        print("[EladCh.kitexts.command.library] EladCh kitexts command library shutdown")
+
+def get_selection() -> List[str]:
+    """Get the list of currently selected prims"""
+    return omni.usd.get_context().get_selection().get_selected_prim_paths()
+
+class ScaleIncrement(omni.kit.commands.Command):
+    def __init__(self, prim_paths: List[str]):
+        self.prim_paths = prim_paths
+        self.stage = omni.usd.get_context().get_stage()
+    
+    def do(self):
+        self.set_scale()
+
+    def undo(self):
+        self.set_scale(True)
+
+    def set_scale(self, undo: bool = False):
+        for path in self.prim_paths:
+            prim = self.stage.GetPrimAtPath(path)
+            old_scale = prim.GetAttribute('xformOp:scale').Get()
+            new_scale = tuple(x + 1 for x in old_scale)
+            if undo:
+                new_scale = tuple(x - 1 for x in old_scale)
+            prim.GetAttribute('xformOp:scale').Set(new_scale)
---
+++ exts/EladCh.kitexts.command.library/docs/index.rst
@@ -0,0 +1,20 @@
+EladCh.kitexts.command.library
+#############################
+
+Example of Python only extension
+
+
+.. toctree::
+   :maxdepth: 1
+
+   README
+   CHANGELOG
+
+
+.. automodule::"EladCh.kitexts.command.library"
+    :platform: Windows-x86_64, Linux-x86_64
+    :members:
+    :undoc-members:
+    :show-inheritance:
+    :imported-members:
+    :exclude-members: contextmanager
